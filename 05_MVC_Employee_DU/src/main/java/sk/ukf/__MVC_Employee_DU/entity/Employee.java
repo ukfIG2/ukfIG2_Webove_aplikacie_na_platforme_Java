4f8533dbd60c1ee0c0b29ef1b24add8ac6b4dd0c
van <van@vam>
2024-11-08 20:26:48 +0100

    entita upraavena

--- 05_MVC_Employee_DU/src/main/java/sk/ukf/__MVC_Employee_DU/entity/Employee.java
+++ 05_MVC_Employee_DU/src/main/java/sk/ukf/__MVC_Employee_DU/entity/Employee.java
@@ -1,6 +1,7 @@
 package sk.ukf.__MVC_Employee_DU.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 import java.util.Objects;
@@ -14,27 +15,52 @@ public class Employee {
     @Column(name = "id")
     private int id;
 
+    @NotBlank(message = "Krstné meno, nemôže byť prázdne.")
+    @Size(min = 2, max = 75, message = "Krstné meno musí mať aspoň 2 až 75 znakov.")
     @Column(name = "first_name")
     private String firstName;
 
+    @NotBlank(message = "Priezvisko, nemôže byť prázdne.")
+    @Size(min = 2, max = 50, message = "Priezvisko musí mať aspoň 2 až 75 znakov.")
     @Column(name = "last_name")
     private String lastName;
 
+    @NotNull(message = "Birth date cannot be null")
+    @Past(message = "Dátum narodenia, musí byť v minulosti.")
     @Column(name = "birth_date")
     private LocalDate birthDate;
 
+    @NotBlank(message = "Email nemôže byť prázdny.")
+    //@Email(message = "Email should be valid")
+    @Pattern(
+            regexp = "^[\\w\\d._%+-]+@[\\w\\d.-]+\\.[A-Za-z]{2,6}$",
+            message = "Email, môže mať iba písmena, čísla a '_'. Email, musí mať formu použivateľské meno'@'doména'.'krajina alebo ina koncovka."
+    )
+    @Size(max = 100, message = "Email musí mať menej ako 100 znakov.")
     @Column(name = "email")
     private String email;
 
+    @NotBlank(message = "Phone number cannot be blank")
+    @Pattern(
+            regexp = "^\\+[0-9]{12}$",
+            message = "Phone number must be in the format '+123456789012' with exactly 12 digits after the '+'"
+    )
     @Column(name = "phone")
     private String phone;
 
+    @NotBlank(message = "Pracovna pozícia, nesmie byť prázdna.")
+    @Size(min = 2, max = 50, message = "Pracovná pozícia, musí mať aspoň 2 až 75 znakov.")
     @Column(name = "job_title")
     private String jobTitle;
 
+    @NotNull(message = "Plat nesmie byť prázdny.")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Plat muší byť viac ako 0,0.")
+    @DecimalMax(value = "100000.0", message = "Plat viac ako 100 000? Koho uplácaš?")
     @Column(name = "salary")
     private Double salary;
 
+    @NotBlank(message = "Typ úvazku, nesmie byť prázdny.")
+    @Size(min = 2, max = 75, message = "Typ úvazku musi byť od 2 do 75 znakov.")
     @Column(name = "full_time")
     private String fullTime;
 
